891c50ed7d9672092a589b0995247ef53b3f03db
Valentyn <49493695+Softsinner@users.noreply.github.com>
2019-12-17 14:33:19 +0200

    Initial commit_4

--- Tableses/ViewController.swift
+++ Tableses/ViewController.swift
@@ -8,47 +8,38 @@
 
 import UIKit
 
-var sections = ["Учні", "Вибули", "Вільні слухачі"]
-
-var students = ["Бондар Павло", "Вождай Ігор", "Демченко Михайло", "Запорожець Максим", "Ілюшенко Ілля", "Nedopaka Alexander", "Таченко Дмитро", "Гуріненко Валентин"]
-
-var off = ["Горошнюк Вячеслав", "БЕРЕЗА МАРИНА",]
-
-var free = ["Пухлій Віталій", "Сагайдак Ілля", "Шурман Андрій", "Лавренко Віталій", "Братчикова Дар'я", "Крістіна",]
+private var sections = ["Учні", "Вибули", "Вільні слухачі"]
+private var students = ["Бондар Павло", "Вождай Ігор", "Демченко Михайло", "Запорожець Максим", "Ілюшенко Ілля", "Nedopaka Alexander", "Таченко Дмитро", "Гуріненко Валентин"]
+private var off = ["Горошнюк Вячеслав", "БЕРЕЗА МАРИНА"]
+private var free = ["Пухлій Віталій", "Сагайдак Ілля", "Шурман Андрій",
+            "Лавренко Віталій", "Братчикова Дар'я", "Крістіна"]
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
+
 }
-  
 // MARK: - UITableViewDataSource
-    extension ViewController: UITableViewDataSource {
-        
+extension ViewController: UITableViewDataSource {
         func numberOfSections(in tableView: UITableView) -> Int {
             3
-        }
-        
+}
         func tableView(_ tableView: UITableView, numberOfRowsInSection  section: Int) -> Int {
-                switch section {
-                case 0:
-                    return 8
-                case 1:
-                    return 2
-                default:
-                    return 6
+            switch section {
+            case 0:
+                return 8
+            case 1:
+                return 2
+            default:
+                return 6
                 }
             }
-    
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
             var identifire = indexPath.section == 0 ? "BasicCell" : "BasicCell1"
             if indexPath.section == 2 {
                 identifire = "BasicCell2"
             }
-            
             let cell = tableView.dequeueReusableCell(withIdentifier: identifire, for: indexPath)
-            
             switch indexPath.section {
             case 0:
                 cell.textLabel?.text = students[indexPath.row]
@@ -60,21 +51,17 @@ class ViewController: UIViewController {
             return cell
         }
     }
-    
 
 // MARK: - UITableViewDelegate
     extension ViewController: UITableViewDelegate {
         func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-                switch section {
-                case 0:
-                    return "\(sections[0])"
-                case 1:
-                    return "\(sections[1])"
-                default:
-                    return "\(sections[2])"
+            switch section {
+            case 0:
+                return "\(sections[0])"
+            case 1:
+                return "\(sections[1])"
+            default:
+                return "\(sections[2])"
                 }
             }
     }
-    
-
-
